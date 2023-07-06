9fe00b6bc7fb50fb6f5d9ef88abcb783e745cec8
RGun44* <ricogunawan444@gmail.com*>
2023-07-06 10:38:10 +0700

    Sudah bisa add, but not auto refresh

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'landing_page.dart';
-import 'login_page.dart';
-import 'register_page.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
+
+import 'package:flutter_application_3/landing_page.dart';
+import 'package:flutter_application_3/login_page.dart';
+import 'package:flutter_application_3/register_page.dart';
 
 class HomePage extends StatefulWidget {
   final String token;
@@ -49,6 +52,88 @@ class _HomePageState extends State<HomePage> {
     return '$baseUrl$relativeUrl';
   }
 
+  Future<void> _addStudent(
+      String name, int age, String profilePicturePath) async {
+    try {
+      final url = Uri.parse('http://localhost:8000/api/students');
+      final request = http.MultipartRequest('POST', url);
+      request.headers['Authorization'] = 'Bearer ${widget.token}';
+      request.fields['student_name'] = name;
+      request.fields['student_age'] = age.toString();
+      if (profilePicturePath.isNotEmpty) {
+        request.files.add(await http.MultipartFile.fromPath(
+          'profile_picture',
+          profilePicturePath,
+        ));
+      }
+
+      final response = await http.Response.fromStream(await request.send());
+
+      if (response.statusCode == 201) {
+        // Student added successfully
+        fetchData(); // Refresh the student list
+        setState(() {});
+      } else {
+        print('Failed to add student. Error: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Exception occurred while adding student: $e');
+    }
+  }
+
+  Future<void> _showAddStudentDialog(BuildContext context) async {
+    String name = '';
+    int age = 0;
+    File? profilePicture;
+
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Add Student'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: 'Name'),
+                onChanged: (value) => name = value,
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Age'),
+                keyboardType: TextInputType.number,
+                onChanged: (value) => age = int.tryParse(value) ?? 0,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final pickedImage = await ImagePicker().pickImage(
+                    source: ImageSource.gallery,
+                  );
+                  if (pickedImage != null) {
+                    profilePicture = File(pickedImage.path);
+                  }
+                },
+                child: Text('Upload Profile Picture'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                _addStudent(name, age, profilePicture?.path ?? '');
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,6 +153,10 @@ class _HomePageState extends State<HomePage> {
             subtitle: Text('Age: ${student['student_age']}'),
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showAddStudentDialog(context),
+        child: Icon(Icons.add),
       ),
     );
   }
